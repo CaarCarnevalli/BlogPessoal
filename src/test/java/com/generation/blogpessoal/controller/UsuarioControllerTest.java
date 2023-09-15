76e2b21a2452adfe67fbfd5fb43152bc2ca61b3a
unknown <carolinascarnevali@gmail.com>
2023-09-15 18:05:42 -0300

    ControllerTeste

---
+++ src/test/java/com/generation/blogpessoal/controller/UsuarioControllerTest.java
@@ -0,0 +1,113 @@
+package com.generation.blogpessoal.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.generation.blogpessoal.model.Usuario;
+import com.generation.blogpessoal.repository.UsuarioRepository;
+import com.generation.blogpessoal.service.UsuarioService;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UsuarioControllerTest {
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@BeforeAll
+	void start(){
+
+		usuarioRepository.deleteAll();
+
+		usuarioService.cadastrarUsuario(new Usuario(0L, 
+			"Root", "root@root.com", "rootroot", "", null));
+
+	}
+	@Test
+	@DisplayName("Cadastrar Um Usuário")
+	public void deveCriarUmUsuario() {
+
+		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(new Usuario(0L, 
+			"Ana julia", "JuliaJulia@email.com.br", "87695043", "-",null));
+
+		ResponseEntity<Usuario> corpoResposta = testRestTemplate
+			.exchange("/usuarios/cadastrar", HttpMethod.POST, corpoRequisicao, Usuario.class);
+
+		assertEquals(HttpStatus.CREATED, corpoResposta.getStatusCode());
+
+	}	
+	
+	@Test
+	@DisplayName("Não deve permitir duplicação do Usuário")
+	public void naoDeveDuplicarUsuario() {
+
+		usuarioService.cadastrarUsuario(new Usuario(0L, 
+			"Gabriela Martins", "GabiMartins@email.com", "12345678", "-",null));
+
+		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(new Usuario(0L, 
+			"Gabriela Martins", "GabiMartis@email.com", "12345678", "-",null));
+
+		ResponseEntity<Usuario> corpoResposta = testRestTemplate
+			.exchange("/usuarios/cadastrar", HttpMethod.POST, corpoRequisicao, Usuario.class);
+
+		assertEquals(HttpStatus.BAD_REQUEST, corpoResposta.getStatusCode());
+	}
+	
+	@Test
+	@DisplayName("Atualizar um Usuário")
+	public void deveAtualizarUmUsuario() {
+
+		Optional<Usuario> usuarioCadastrado = usuarioService.cadastrarUsuario(new Usuario(0L, 
+			"Carolina Carnevali", "CarolCar@gmail.com", "Carol666", "-",null));
+
+		Usuario usuarioUpdate = new Usuario(usuarioCadastrado.get().getId(), 
+			"Lucas Motta", "Lucas_Motta@email.com.br", "Lucas999" , "-",null);
+		
+		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(usuarioUpdate);
+
+		ResponseEntity<Usuario> corpoResposta = testRestTemplate
+			.withBasicAuth("root@root.com", "rootroot")
+			.exchange("/usuarios/atualizar", HttpMethod.PUT, corpoRequisicao, Usuario.class);
+
+		assertEquals(HttpStatus.OK, corpoResposta.getStatusCode());
+		
+	}
+	
+
+@Test
+@DisplayName("Listar todos os Usuários")
+public void deveMostrarTodosUsuarios() {
+
+	usuarioService.cadastrarUsuario(new Usuario(0L, 
+		"Sophia Martins", "SophiaMartins@email.com.br", "sho9865", "-",null));
+	
+	usuarioService.cadastrarUsuario(new Usuario(0L, 
+		"Gustavo Martins", "GustavoMartins@email.com.br", "Gus1234", "-",null));
+
+	ResponseEntity<String> resposta = testRestTemplate
+	.withBasicAuth("root@root.com", "rootroot")
+		.exchange("/usuarios/all", HttpMethod.GET, null, String.class);
+
+	assertEquals(HttpStatus.OK, resposta.getStatusCode());
+
+}}
